9a19cd4c880f91cff4b3600f27f9723b3725a30c
Tom <tomer.tsuker@gmail.com>
2020-06-28 14:13:06 +0900

    latest update

--- js/apps/note-app/comps/Shared components/header.cmp.js
+++ js/apps/note-app/comps/Shared components/header.cmp.js
@@ -10,8 +10,7 @@ export default {
       <div class="apps-container">
         <span @click="showDropDown =!showDropDown" tag="button" to="">Apps</span>
         <div v-if="showDropDown" class="app-menu">
-        <router-link  class="fas fa-book-open" tag="i" to="/book">
-        </router-link>
+     
         <router-link class="far fa-envelope" tag="i" to="/email/list/isInbox"></router-link>
         <router-link v-if="$route.path!='/note'" class="far fa-sticky-note" tag="i" to="/note"></router-link>
         </div>
@@ -33,6 +32,7 @@ export default {
   created() {},
   computed: {
     iconClass() {
+      //   <router-link  class="fas fa-book-open" tag="i" to="/book"></router-link>
       if (this.iconType === "email") {
         return "far fa-envelope";
       } else {
